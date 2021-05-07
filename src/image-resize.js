8772f41d8090c9c5c52eb55f0eaa4d83f363ce24
DeryckJackson <dgjackson01@gmail.com>
2021-05-07 14:13:22 -0700

    post sticker route

--- src/image-resize.js
+++ src/image-resize.js
@@ -1,15 +1,16 @@
 const sharp = require('sharp');
 
-exports.resize = (file) => {
+exports.resize = (tmpDir, file) => {
   const image = sharp(file)
   const metadata = image.metadata()
+
   if (metadata.height > metadata.width) {
     image
       .resize({ height: 512 })
-      .toFile('./temp/temp.png')
+      .toFile(`./${tmpDir}/temp.png`)
   } else {
     image
       .resize({ width: 512 })
-      .toFile('./temp/temp.png')
+      .toFile(`./${tmpDir}/temp.png`)
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 require('dotenv').config()
+const fs = require('fs')
 
 const express = require('express')
 const cors = require('cors')
@@ -6,10 +7,12 @@ const multer = require('multer')
 
 const axios = require('axios')
 const resize = require('./image-resize.js')
+const { dir } = require('console')
 
 const upload = multer()
 
 const app = express()
+
 app.use(cors())
 const port = 3000
 
@@ -41,8 +44,36 @@ app.get('/stickers/:name', async (req, res) => {
   }
 })
 
-app.post('/stickers/:name', (req, res) => {
+app.post('/stickers/:name/:emojis', upload.single('photo') , (req, res) => {
+  
 
+  const userId = process.env.USER_ID
+  const name = req.params.name
+  const emojis = req.params.emojis
+
+  const tmpDir = fs.mkdtemp('foo-', (err, directory) => {
+    if (err) throw err
+    
+    resize.resize(directory, req.file.buffer)
+    
+    return directory
+  })
+
+  console.log(tmpDir)
+
+  // fs.rmdir(directory, (err) => {
+  //   if (err) throw err
+  // })
+
+  // try {
+  //   const response = await axios.post(`/addStickerToSet?name=${name}&user_id=${userId}emojis=${emojis}`, formData)
+
+
+  // } catch (err) {
+  //   console.log(err)
+  // }
+
+  res.status(202).send('Accepted')
 })
 
 app.listen(port, () => {
