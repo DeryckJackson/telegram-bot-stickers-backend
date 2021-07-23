dcfd55e6c442cfa74b8b9f907abcfb96bba08464
Jerax <dgjackon01@gmail.com>
2021-07-22 19:18:43 -0700

    gcloud changes

--- src/image-resize.js
+++ src/image-resize.js
@@ -1,16 +1,18 @@
 const sharp = require('sharp');
 
-exports.resize = async (tmpDir, file) => {
+exports.resize = async (file) => {
   const image = sharp(file)
   const metadata = await image.metadata()
 
   if (metadata.height > metadata.width) {
     return image
       .resize({ height: 512 })
-      .toFile(`./${tmpDir}/temp.png`)
+      .toFormat('png')
+      .toBuffer()
   } else {
     return image
       .resize({ width: 512 })
-      .toFile(`./${tmpDir}/temp.png`)
+      .toFormat('png')
+      .toBuffer()
   }
 }
